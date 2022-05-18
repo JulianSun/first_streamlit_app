54ae1debc22778c34ca1b1b56468b720c9172792
JulianSun <xin.julian.sun@gmail.com>
2022-05-19 02:41:39 +0800

    Update streamlit_app.py
    Add some menu stuff.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("Here is my title")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
