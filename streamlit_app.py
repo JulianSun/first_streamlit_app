d5e4baeb391f5607a87be26f22017782215fb609
JulianSun <xin.julian.sun@gmail.com>
2022-05-20 05:29:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,9 +49,9 @@ except URLError as e:
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+my_cur.execute("select * from FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The Fruit List Contains")
 streamlit.text(my_data_row)
 
 
