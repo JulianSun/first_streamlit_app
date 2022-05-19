d1ba9a86a520e82f197f91c9f95b3370d64e1413
JulianSun <xin.julian.sun@gmail.com>
2022-05-19 12:51:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('what fruit would you like information about?','Kiwi)
+fruit_choice = streamlit.text_input('what fruit would you like information about?','Kiwi')
 streamlit.write('The user entered', fruit_choice)
 import requests
 fruityvice_response = requests.get("http://fruityvice.com/api/fruit/"+ fruit_choice)
